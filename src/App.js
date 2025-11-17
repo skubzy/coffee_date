c8820dd950c1ba181a23de36ce036dc9a049a553
skubzy <paul.chukwu.official@gmail.com>
2025-11-16 19:54:53 -0500

    7:54 11-16

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="Coffee Dates">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo1" />
         <p>
           Edit <code>src/App.js</code> Testing to work.
         </p>
