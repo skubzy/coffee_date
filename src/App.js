2441751c27759f286ef083b03ca817a8df1a59de
skubzy <paul.chukwu.official@gmail.com>
2025-11-16 20:42:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo1" />
         <p>
-          Edit <code>src/App.js</code> Testing to work.
+           <code>Coffee Dates</code> 
         </p>
         <a
           className="App-link"
