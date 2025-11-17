d2b29796beb395a6b128f9f311a2c2aef8f4b3f5
skubzy <paul.chukwu.official@gmail.com>
2025-11-16 20:01:55 -0500

    8:01  11-16

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 //import Header from "./components/Header";
 //import ItemList from "./components/ItemList";
 
-import logo from './logo.svg';
+import logo from './logo1.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="Coffee Dates">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo1" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> Testing to work.
         </p>
